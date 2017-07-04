bfc0e8e92b48bcc15e6a512025b05f07ad3c8c8e
Alexey Krukov <krukovworkmail@gmail.com>
2017-07-04 16:02:11 +0300

    second version

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let express = require('express'),
     app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-//console.log(mongoose.version);
 let db = mongoose.createConnection('mongodb://localhost:27017/House');
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function callback () {
@@ -25,16 +24,71 @@ newOwner.save(function (err, newOwner) {
     else {
         console.log("Yes");
         }
+res.send("");
 });
 });
 app.get("/", function(req, res){
     res.render('new.ejs');
 });
 app.get("/delete", function(req, res){
-    //res.render('delete.ejs');
     Owner.find(function (err, owners) {
-        res.send(owners);
-        //console.log(owners);
-    })
+            if(owners.length == 0)
+                res.write("No users");
+            else {
+                for(let i = 0; i < owners.length; i++)
+                    res.write((i + 1) + " " + owners[i]["Owner"] + " " + owners[i]["Appartment"] + " " + owners[i]["Job"] + "<br>");
+            }
+        });
+    res.write("<html>");
+    res.write("<head>");
+        res.write("<title>Delete</title>");
+    res.write("</head>");
+    res.write("<body>");
+        res.write("<form action=\"/delete\" method=\"post\">");
+            res.write("<label for=\"delete_id\">ID</label>");
+            res.write("<br>");
+            res.write("<input type=\"text\" name = \"delete_id\" id = \"delete_id\">");
+            res.write("<br><br>");
+           res.write("<button>Send</button>");
+        res.write("</form>");
+   res.write("</body>");
+res.write("</html>");
+    //res.send("");
+});
+app.post("/delete", function(req, res){
+    Owner.find(function (err, owners) {
+            if(owners.length == 0)
+                res.write("No users");
+            else {
+                        let id = req.body.delete_id - 1;
+                        console.log(owners[id]);
+                        console.log(req.body.delete_id);
+                        Owner.findOneAndRemove({'Appartment' : owners[id]["Appartment"]}, function(owners, err){});
+            res.redirect('/delete_success/' + req.body.delete_id);
+            }
+})});
+app.get("/delete_success/:id", function(req, res){
+        Owner.find(function (err, owners) {
+            if(owners.length == 0)
+                res.write("No users");
+            else {
+                for(let i = 0; i < owners.length; i++)
+                    res.write((i + 1) + " " + owners[i]["Owner"] + " " + owners[i]["Appartment"] + " " + owners[i]["Job"] + "<br>");
+            }
+        });
+        res.write("<html>");
+    res.write("<head>");
+        res.write("<title>Delete</title>");
+    res.write("</head>");
+    res.write("<body>");
+        res.write("<form action=\"/delete\" method=\"post\">");
+            res.write("<label for=\"delete_id\">ID</label>");
+            res.write("<br>");
+            res.write("<input type=\"text\" name = \"delete_id\" id = \"delete_id\">");
+            res.write("<br><br>");
+           res.write("<button>Send</button>");
+        res.write("</form>");
+   res.write("</body>");
+res.write("</html>");
 });
 app.listen(8080);
